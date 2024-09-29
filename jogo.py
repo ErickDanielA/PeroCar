9dddfeab348248f1d636dce2baaf660972c5bc62
erickdaniela <danielkewem2018@gmail.com>
2024-09-28 23:33:14 -0300

    Carros funcionando 100%

--- jogo.py
+++ jogo.py
@@ -1,20 +1,32 @@
 ﻿import pygame
+from random import randint
 
 pygame.init()
 x = 119 + 125 + 110
-y = 300
+y = 200
 
 pos_x = 119
-pos_y = 300
 
-velocidade = 15
+pos_y_1 = 800
+pos_y_2 = 800
+pos_y_3 = 800
+pos_y_4 = 800
+
+velocidade = 12
+
 
 
 fundo = pygame.image.load('carros/background-1_0.png')
-carro = pygame.image.load('carros/carro_red.png')
-carronpc = pygame.image.load("carros/carro_verde.png")
-carronpc2 = pygame.image.load("carros/carro_amarelo.png")
-velocidade_npc = 20
+carro = pygame.image.load('carros/carro_vermelho.png')
+carronpc = pygame.image.load("carros/carro_amarelo.png")
+carronpc2 = pygame.image.load("carros/carro_verde.png")
+carro_preto = pygame.image.load("carros/carro_preto.png")
+carro_policia = pygame.image.load("carros/carro_policia.png")
+
+velocidade_npc1 = 10
+velocidade_npc2 = 11
+velocidade_npc3 = 20
+velocidade_npc4 = 9
 
 janela = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Jogo")
@@ -33,9 +45,11 @@ while janela_aberta:
 
     janela.blit(fundo, (0, 0))
     janela.blit(carro, (x, y))
-    janela.blit(carronpc, (pos_x, pos_y))
-    janela.blit(carronpc2, (pos_x + 375, pos_y))
-    
+    janela.blit(carronpc, (pos_x, pos_y_1))
+    janela.blit(carronpc2, (pos_x + 375, pos_y_2))
+    janela.blit(carro_preto, (pos_x + 175, pos_y_3))
+    janela.blit(carro_policia, (pos_x + 255, pos_y_4))
+
     # pygame.draw.circle(janela, (0, 255, 0), (x, y), 50)
     pygame.display.update()
 
@@ -49,10 +63,19 @@ while janela_aberta:
     if comandos[pygame.K_LEFT]:
         x -= velocidade
     
-    if pos_y <= -250:
-        pos_y = 600
+    if pos_y_1 <= -250 and pos_y_2 <= -250 and pos_y_3 <= -250:
+        pos_y_1 = randint(600, 1500)
+        pos_y_2 = randint(600, 1500)
+        pos_y_3 = randint(600, 1500)
+        pos_y_4 = randint(600, 1500)
 
-    pos_y -= velocidade_npc
+    if y <= -250:
+        y = 600
+
+    pos_y_1 -= velocidade_npc1
+    pos_y_2 -= velocidade_npc2
+    pos_y_3 -= velocidade_npc3
+    pos_y_4 -= velocidade_npc4
 
 pygame.quit()
     
