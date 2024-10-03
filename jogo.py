9ac7fa4ce90e632a94b21a5fead277d79e24f654
erickdaniela <danielkewem2018@gmail.com>
2024-10-03 00:00:14 -0300

    limite para carros

--- jogo.py
+++ jogo.py
@@ -1,8 +1,10 @@
-﻿import pygame
+﻿
+import pygame
 from random import randint
 
 pygame.init()
-x = 119 + 125 + 110
+# x = 119 + 125 + 110
+x = 470
 y = 200
 
 pos_x = 119
@@ -24,9 +26,9 @@ carro_preto = pygame.image.load("carros/carro_preto.png")
 carro_policia = pygame.image.load("carros/carro_policia.png")
 
 velocidade_npc1 = 10
-velocidade_npc2 = 11
-velocidade_npc3 = 20
-velocidade_npc4 = 9
+velocidade_npc2 = 10
+velocidade_npc3 = 10
+velocidade_npc4 = 10
 
 janela = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Jogo")
@@ -51,8 +53,7 @@ while janela_aberta:
     janela.blit(carro_policia, (pos_x + 255, pos_y_4))
 
     # pygame.draw.circle(janela, (0, 255, 0), (x, y), 50)
-    pygame.display.update()
-
+    
     comandos = pygame.key.get_pressed()
     if comandos[pygame.K_UP]:
         y -= velocidade
@@ -63,19 +64,31 @@ while janela_aberta:
     if comandos[pygame.K_LEFT]:
         x -= velocidade
     
-    if pos_y_1 <= -250 and pos_y_2 <= -250 and pos_y_3 <= -250:
+    if pos_y_1 <= -250:
         pos_y_1 = randint(600, 1500)
+    if pos_y_2 <= -250:
         pos_y_2 = randint(600, 1500)
+    if pos_y_3 <= -250:
         pos_y_3 = randint(600, 1500)
+    if pos_y_4 <= -250:
         pos_y_4 = randint(600, 1500)
-
+        
     if y <= -250:
         y = 600
+        
+    # Restrição de X para o jogador
+    if x <= 90:
+        x = 90
+    if x <= 470:
+        x = 470
+        
 
     pos_y_1 -= velocidade_npc1
     pos_y_2 -= velocidade_npc2
     pos_y_3 -= velocidade_npc3
     pos_y_4 -= velocidade_npc4
+    
+    pygame.display.update()
 
 pygame.quit()
     
